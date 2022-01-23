84f06c07d4ac026747629e41f2296d930707d4cf
jinhobyeong <jhb_0921@naver.com>
2022-01-24 03:08:17 +0900

    last assign

--- js/background.js
+++ js/background.js
@@ -1,9 +1,5 @@
 const images = ["0.png","1.png","2.png"];
-
+const body1 = document.getElementById("bodyImg");
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-//ccreatElement 가 뭘까
-bgImage.src = `img/${chosenImage}`;
-//html를 js에서 다룬것
-document.body.appendChild(bgImage); //body에 추가해주는것
+body1.style = `background-image: url('img/${chosenImage}');  background-size:cover; background-repeat: no-repeat;`;
--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,18 @@
-const API_KEY="241051bf13976dd3ddff8b8d9f247255e";
+const API_KEY="56893d8f167163b0c753d8893218d24c";
 
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("you live in ", lat, lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    fetch(url);
+    // console.log("you live in ", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data =>{
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText =`${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
     alert("Can't find you. No wather for you");
